8340a0e4fcbe2977f6e0181ad1b87c68ec75d4c6
BouH1986 <jack-2006@mail.ru>
2026-08-13 16:40:23 +0400

    write

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -21,12 +21,18 @@ public class ToDoList {
     public void delIndex(int numTask) {
         if (numTask > 0 && numTask < toDoList.size()) {
             toDoList.remove(numTask - 1);
-        }else {
-            System.out.println("Не верный номер задачи");
+            System.out.println("Удалено!");
+        } else {
+            System.out.println("Неверный номер задачи");
         }
     }
 
     public void delName(String task) {
-        toDoList.remove(task);
+        if (toDoList.contains(task)) {
+            toDoList.remove(task);
+            System.out.println("Удалено!");
+        } else {
+            System.out.println("Неверное название задачи");
+        }
     }
 }
